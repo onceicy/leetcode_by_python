9533680746aa8619906d6cd39f23a65c808e6a49
onceicy <1803105512@qq.com>
2020-07-24 22:58:36 +0800

    solution2

---
+++ Solution2.py
@@ -0,0 +1,18 @@
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        stack = ['#']
+        for temp in s:
+            if temp == ')':
+                if stack.pop() != '(':
+                    return False
+            elif  temp == ']':
+               if stack.pop() != '[':
+                    return False                      
+            elif  temp == '}':
+               if stack.pop() != '{':
+                    return False                       
+            else :
+                stack.append(temp)
+        if stack.pop() != '#':
+            return False
+        return True
