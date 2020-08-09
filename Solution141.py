e90dd22f2114983bc38dbc5d9d2d3e5438927a24
onceicy <1803105512@qq.com>
2020-08-09 22:37:38 +0800

    solve some problems

---
+++ Solution141.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution141:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head == None or head.next == None:  
+            return False
+        fast = head.next
+        slow = head
+        while slow != fast:  
+            if fast == None or fast.next == None:
+                return False           
+            slow = slow.next
+            fast = fast.next.next
+        return True
